fa0b9d9e01f01821a4f9502a6d93fe9b47fddb7f
Derrick <derrickrlewis@gmail.com>
2020-10-18 00:35:23 -0500

    Added permissions

--- app/src/main/java/io/figure8/musicplayerapp/MainActivity.java
+++ app/src/main/java/io/figure8/musicplayerapp/MainActivity.java
@@ -2,6 +2,11 @@ package io.figure8.musicplayerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.ActivityManager;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +15,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    private static final String[] PERMISSIONS = {
+            Manifest.permission.READ_EXTERNAL_STORAGE
+    };
+
+    private static final int REQUEST_PERMISSIONS = 12345;
+    private static final int PERMISSION_COUNT =1;
+
+    @SuppressLint("NewApi")
+    private boolean arePermissionsDenied(){
+        for (int i = 0; i < PERMISSION_COUNT; i++){
+            if (checkSelfPermission(PERMISSIONS[i]) != PackageManager.PERMISSION_GRANTED){
+                return true;
+            }
+        }
+        return false;
+    }
+    @SuppressLint("NewApi")
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String[] permissions ,int[] grantResults ){
+        super.onRequestPermissionsResult(requestCode ,permissions,grantResults);
+       if (arePermissionsDenied()){
+           ((ActivityManager) (this.getSystemService(ACTIVITY_SERVICE))).clearApplicationUserData();
+           recreate();
+       }else{
+           onResume();
+       }
+    }
+
+    @Override
+    protected void  onResume(){
+        super.onResume();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && arePermissionsDenied()){
+            requestPermissions(PERMISSIONS,REQUEST_PERMISSIONS);
+            return;
+        }
+
     }
 }
